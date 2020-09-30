1c3190ddb3628773a8a0404713adf5abc6913caa
Doug Wright <dw@nof49.ca>
2020-09-30 14:30:33 -0400

    Completed exercise 5

--- __workshop/_fundamentals/__tests__/exercise-5.test.js
+++ __workshop/_fundamentals/__tests__/exercise-5.test.js
@@ -4,7 +4,9 @@ const addNumbers = require("../exercise-5");
 // Write 3 more expect functions to test the function you just wrote.
 test("Exercise 5", () => {
   expect(addNumbers(1, 2, 3, 4, 5)).toBe(55);
-  // add more tests here...
+  expect(addNumbers()).toBe(undefined);
+  expect(addNumbers(1, "2", 3, "4", 5)).toBe(35);
+  expect(addNumbers(1, 1, 1)).toBe(3);
 });
 
 // More info on jest expect: https://jestjs.io/docs/en/expect
