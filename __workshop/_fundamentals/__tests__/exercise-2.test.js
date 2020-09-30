07f0fa3e93eb1b741352d4e3bdc3ede6cc48b397
Doug Wright <dw@nof49.ca>
2020-09-30 10:15:02 -0400

    Completed exercise 2

--- __workshop/_fundamentals/__tests__/exercise-2.test.js
+++ __workshop/_fundamentals/__tests__/exercise-2.test.js
@@ -4,7 +4,9 @@ const redacted = require("../exercise-2");
 // Write 3 more expect functions to test the function you just wrote.
 test("Exercise 2", () => {
   expect(redacted(["bacon", false, 42])).toStrictEqual(["redacted", false, 42]);
-  // add more tests here...
+  expect(redacted(["eggs", 100, "100"])).toStrictEqual(["redacted", 100, "redacted"]);
+  expect(redacted([1, 2, 3])).toStrictEqual([1, 2, 3]);
+  expect(redacted(["foo", "bar"])).toStrictEqual(["redacted", "redacted"]);
 });
 
 // More info on jest expect: https://jestjs.io/docs/en/expect
