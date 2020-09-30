b111ccc96da099276aec06ec11ed21893b9c4eb8
Sandra <sandrabess@hotmail.com>
2020-09-30 15:51:40 -0400

    ex 2 completed

--- __workshop/_fundamentals/__tests__/exercise-2.test.js
+++ __workshop/_fundamentals/__tests__/exercise-2.test.js
@@ -5,6 +5,11 @@ const redacted = require("../exercise-2");
 test("Exercise 2", () => {
   expect(redacted(["bacon", false, 42])).toStrictEqual(["redacted", false, 42]);
   // add more tests here...
+  expect(redacted([["bacon"], false, 42])).toStrictEqual([["bacon"], false, 42]);
+  expect(redacted(["bacon", "test", "42"])).toStrictEqual(["redacted", "redacted", "redacted"]);
+  expect(redacted([])).toStrictEqual([]);
+  expect(redacted([""])).toStrictEqual(["redacted"]);
+  expect(redacted(45)).toBe(undefined);
 });
 
 // More info on jest expect: https://jestjs.io/docs/en/expect
