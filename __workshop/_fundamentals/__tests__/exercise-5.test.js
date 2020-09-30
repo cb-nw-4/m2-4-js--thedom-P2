642b1fc380832f3ddedc3c0282bdc82826492cd6
chris shino <chris.shino@hotmail.com>
2020-09-29 20:33:11 -0400

    done 5

--- __workshop/_fundamentals/__tests__/exercise-5.test.js
+++ __workshop/_fundamentals/__tests__/exercise-5.test.js
@@ -5,6 +5,7 @@ const addNumbers = require("../exercise-5");
 test("Exercise 5", () => {
   expect(addNumbers(1, 2, 3, 4, 5)).toBe(55);
   // add more tests here...
+  expect(addNumbers()).toBe('undefined')
 });
 
 // More info on jest expect: https://jestjs.io/docs/en/expect
