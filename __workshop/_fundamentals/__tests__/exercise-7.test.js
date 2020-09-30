dc0842f3d2603bd4d4b7513094a4cc1c042c75e0
ayasatograph@icloud.com <ayasatography@icloud.com>
2020-09-30 18:53:49 -0400

    Exercise-7 answered

--- __workshop/_fundamentals/__tests__/exercise-7.test.js
+++ __workshop/_fundamentals/__tests__/exercise-7.test.js
@@ -17,7 +17,43 @@ test("Exercise 7", () => {
     { name: "liv", age: 36, isAvailable: true },
     { name: "dave", age: 43, isAvailable: true },
   ]);
+  
   // add more tests here...
+  expect(
+    addValues(
+      ["chris,  23", "liv, 36", "dave, 43"],
+      { isAvailable: true }
+    )
+  ).toStrictEqual(undefined);
+  expect(
+    addValues("Chris who is 23 years old",
+      { isAvailable: true }
+    )
+  ).toStrictEqual(undefined);
+  expect(
+    addValues(
+      [
+        { name: "chris", age: 23 },
+        { name: "liv", age: 36 },
+        { name: "dave", age: 43 },
+      ],
+      "is available"
+    )
+  ).toStrictEqual(undefined);
+  expect(
+    addValues(
+      [
+        { name: "chris", age: 23, nationality: "Canadian" },
+        { name: "liv", age: 36 },
+        { name: "dave", age: 43, nationality: "American", isEmployed: true },
+      ],
+      { isAvailable: "on Mondays" }
+    )
+  ).toStrictEqual([
+    { name: "chris", age: 23, nationality: "Canadian", isAvailable: "on Mondays" },
+    { name: "liv", age: 36, isAvailable: "on Mondays" },
+    { name: "dave", age: 43, nationality: "American", isEmployed: true, isAvailable: "on Mondays" },
+  ]);
 });
 
 // More info on jest expect: https://jestjs.io/docs/en/expect
