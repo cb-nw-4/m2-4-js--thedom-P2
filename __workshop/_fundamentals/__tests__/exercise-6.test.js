85f5072d48488c727c98cac60616590f837e9089
Sandra <sandrabess@hotmail.com>
2020-09-30 20:04:59 -0400

    ex 6 completed

--- __workshop/_fundamentals/__tests__/exercise-6.test.js
+++ __workshop/_fundamentals/__tests__/exercise-6.test.js
@@ -5,6 +5,10 @@ const isPalindrome = require("../exercise-6");
 test("Exercise 6", () => {
   expect(isPalindrome("radar")).toBe(true);
   // add more tests here...
+  expect(isPalindrome(123)).toBe(undefined);
+  expect(isPalindrome("")).toBe(true);
+  expect(isPalindrome("hello")).toBe(false);
+  expect(isPalindrome("radar radar")).toBe(true);
 });
 
 // More info on jest expect: https://jestjs.io/docs/en/expect
