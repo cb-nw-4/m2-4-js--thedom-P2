afb1d616f9ef2ac96fea96fea5886c9c14735289
Sandra <sandrabess@hotmail.com>
2020-10-01 14:16:46 -0400

    exercises 1.1 completed

--- __workshop/exercise-1/app.js
+++ __workshop/exercise-1/app.js
@@ -1,2 +1,19 @@
 // Preset values
 const FROGS = 3;
+
+// Exercise 1.1 - Set up the track
+let track = document.querySelector(".track");
+for (let count = 1; count <= FROGS; count++) {  
+  // create a lane (<li>)
+  let lane = document.createElement("li");
+  // give lane an id
+  lane.id = `lane-<${count}>`
+  // create a <span>
+  let mySpan = document.createElement("span");
+  // add the lane number to the span
+  mySpan.innerHTML = `${count}`;
+  // append the span to the lane
+  lane.appendChild(mySpan);
+  // append the lane to the track
+  track.appendChild(lane);
+}
