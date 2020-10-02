eeb20cae1ca9f6482b6ea9c2235c5fd734d215dc
Vanessa Chan <chw.vanessa@gmail.com>
2020-10-01 22:56:15 -0400

    Ex 1.4

--- __workshop/exercise-1/app.js
+++ __workshop/exercise-1/app.js
@@ -29,12 +29,24 @@ let newID="";
 
     racers.forEach(function(element,i) {
         frog = document.createElement("span");
-        frog.class = "frog";
+        frog.className = "frog";
         frog.innerText = `${element.name} (${element.number})`;
         frog.style.backgroundColor = element.color;
         document.getElementById(laneString.concat(i+1)).appendChild(frog);
     });
 
+//Exercise 1.4
+
+//let theFrog = document.getElementById("lane-1").getElementsByClassName("frog");
+let frogName;
+
+racers.forEach(function(element,i){
+    frogName = document.createElement("span");
+    frogName.innerText = `${element.name}`;
+    frogName.className = "frog-name";
+    document.getElementById(laneString.concat(i+1)).lastChild.innerText = `${element.number}`;
+    document.getElementById(laneString.concat(i+1)).appendChild(frogName);
+});
 
 
     
