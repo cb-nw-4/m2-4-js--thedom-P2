080d388c11bfce0d666f72ae6117b4d866da6fa2
Vanessa Chan <chw.vanessa@gmail.com>
2020-10-01 20:59:35 -0400

    Ex 1.1

--- __workshop/exercise-1/app.js
+++ __workshop/exercise-1/app.js
@@ -1,2 +1,17 @@
 // Preset values
 const FROGS = 3;
+
+const mainContainer = document.querySelector("main");
+
+// Exercise 1.1 Set up
+let lane, laneNumber;
+for(let i = 1; i<=FROGS; i++){
+
+    lane = document.createElement("li");
+    lane.id = `lane-${i}`;
+    laneNumber = document.createElement("span");
+    laneNumber.innerText = `${i}`;
+    
+    lane.appendChild(laneNumber);
+    document.getElementById("track").appendChild(lane);
+}
