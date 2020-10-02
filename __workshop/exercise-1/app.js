b12f56155b899fc79c64716379f5d6a2746b7d84
chris shino <chris.shino@hotmail.com>
2020-10-01 22:09:42 -0400

    done to 1.7

--- __workshop/exercise-1/app.js
+++ __workshop/exercise-1/app.js
@@ -1,34 +1,56 @@
 // Preset values
 const FROGS = 3;
-const track = document.querySelector('#track')
+const track = document.querySelector("#track");
 
-
-
-for (let count = 1; count <= FROGS; count++){
-  const lane =  document.createElement('li')
-  lane.id = `lane-${count}`
-  const span = document.createElement('span')
-  span.innerText = count
-  lane.appendChild(span)
-  track.appendChild(lane)
+for (let count = 1; count <= FROGS; count++) {
+  const lane = document.createElement("li");
+  lane.id = `lane-${count}`;
+  const span = document.createElement("span");
+  span.innerText = count;
+  lane.appendChild(span);
+  track.appendChild(lane);
 }
 
-const racers = []
+const racers = [];
 
-for (let i = 0; i < FROGS; i++){
-  racers.push(frogstable[i])
+for (let i = 0; i < FROGS; i++) {
+  frogstable[i].progress = 0;
+  frogstable[i].lane = `lane-${i + 1}`;
+  racers.push(frogstable[i]);
+  // console.log(frogstable[i])
 }
 
-console.log(racers)
+console.log(racers);
 
 racers.forEach((frog, i) => {
-  const laneNode = document.querySelector(`#lane-${i + 1}`)
-  const frogNameNode = document.createElement('span')
-  const frogNumberNode = document.createElement('span')
-  frogNumberNode.innerText = frog.number
-  frogNameNode.innerText = frog.name
-  frogNumberNode.style = `background-color: ${frog.color}`
-  laneNode.appendChild(frogNameNode)
-  laneNode.appendChild(frogNumberNode)
-})
+  const laneNode = document.querySelector(`#lane-${i + 1}`);
+  const frogNameNode = document.createElement("span");
+  const frogNumberNode = document.createElement("span");
+  frogNumberNode.innerText = frog.number;
+  frogNameNode.innerText = frog.name;
+  frogNumberNode.style = `background-color: ${frog.color}`;
+  laneNode.appendChild(frogNameNode);
+  laneNode.appendChild(frogNumberNode);
+  frogNumberNode.className = `frog`;
+  frogNameNode.className = `frog-name`;
+});
+
+function racingFrog(racer) {
+  const trackWidth = track.offsetWidth;
+  const hop = setInterval(function () {
+    const hopLength = Math.floor(((Math.random() * 100) / trackWidth) * 100);
+    racer.progress = racer.progress + hopLength
+    if (racer.progress >= 100) {
+      clearInterval(hop)
+      console.log(`${racer.name} has Finished!`)
+    }
+    // console.log(racer.progress)
+  
+  }, 1000);
+    
+}
+
+racers.forEach((frog) => {
+  racingFrog(frog);
+});
 
